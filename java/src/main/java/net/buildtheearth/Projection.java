626e6919e57745ef8250aea2f181716de4a5e86c
Kyan Van den Eynde <66461508+kyanvde@users.noreply.github.com>
2026-04-26 17:24:55 +0200

    feat(java): Add gradle project structure

---
+++ java/src/main/java/net/buildtheearth/Projection.java
@@ -0,0 +1,12 @@
+package net.buildtheearth;
+
+public class Projection {
+
+    public static void fromGeo() {
+        System.out.println("From Geo");
+    }
+
+    public static void toGeo() {
+        System.out.println("To Geo");
+    }
+}
