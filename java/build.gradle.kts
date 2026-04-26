6bb0ec73e07aa8a9a95732ab30b8a7fd8ec9c88c
Kyan Van den Eynde <66461508+kyanvde@users.noreply.github.com>
2026-04-26 18:49:52 +0200

    feat(java): Add initial java code.

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("com.google.guava:guava:33.6.0-jre")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- java/src/main/java/net/buildtheearth/Projection.java
+++ java/src/main/java/net/buildtheearth/Projection.java
@@ -1,12 +1,90 @@
 package net.buildtheearth;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class Projection {
 
-    public static void fromGeo() {
-        System.out.println("From Geo");
+    private static GeographicProjection projection;
+
+    private static final DecimalFormat decFormat1 = new DecimalFormat();
+
+    static {
+        decFormat1.setMaximumFractionDigits(1);
+        DecimalFormatSymbols usSymbols = new DecimalFormatSymbols(Locale.US);
+        decFormat1.setDecimalFormatSymbols(usSymbols);
+
+        projection  = GeographicProjection.projections.get("bteairocean");
+        projection = GeographicProjection.orientProjection(projection, GeographicProjection.Orientation.upright);
+        projection = new ScaleProjectionTransform(projection, 7318261.522857145, 7318261.522857145);
+        projection = new OffsetProjectionTransform(projection, 0, 0);
     }
 
-    public static void toGeo() {
-        System.out.println("To Geo");
+    /**
+     * Converts Minecraft coordinates to geographic coordinates
+     *
+     * @param xCords - Minecraft player x-axis coordinates
+     * @param yCords - Minecraft player y-axis coordinates
+     * @return - WG84 EPSG:4979 coordinates as double array {lon,lat} in degrees
+     */
+    public static double[] convertToGeo(double xCords, double yCords) {
+        try {
+            double[] res = projection.toGeo(xCords, yCords);
+            return new double[]{res[1], res[0]};
+        } catch (OutOfProjectionBoundsException e) {
+            return new double[]{0., 0.};
+        }
+    }
+
+    /**
+     * Gets in-game coordinates from geographical location
+     *
+     * @param lon Geographical Longitude
+     * @param lat Geographic Latitude
+     * @return The in-game coordinates (x, z)
+     */
+    public static double[] convertFromGeo(double lat, double lon) {
+        try {
+            return projection.fromGeo(lon, lat);
+        } catch (OutOfProjectionBoundsException e) {
+            return new double[]{0., 0.};
+        }
+    }
+
+    /**
+     * Get formatted numeric geographic coordinates
+     *
+     * @param coordinates - WG84 EPSG:4979 coordinates as double array
+     * @return - Formatted numeric coordinates as String
+     */
+    private static String formatGeoCoordinatesNumeric(double[] coordinates) {
+        return coordinates[1] + "," + coordinates[0];
+    }
+
+    /**
+     * Get formatted NSEW geographic coordinates
+     *
+     * @param coordinates - WG84 EPSG:4979 coordinates as double array
+     * @return - Formatted NSEW coordinates as String
+     */
+    private static String formatGeoCoordinatesNSEW(double[] coordinates) {
+        double fixedLon = coordinates[0];
+        double fixedLat = coordinates[1];
+        String eo = fixedLon < 0 ? "W": "E";
+        String ns = fixedLat < 0 ? "S" : "N";
+        double absLon = Math.abs(fixedLon);
+        double absLat = Math.abs(fixedLat);
+        int longitudeDegrees = (int) absLon;
+        int latitudeDegrees = (int) absLat;
+        double minLon = absLon * 60 - longitudeDegrees * 60;
+        double minLat = absLat * 60 - latitudeDegrees * 60;
+        int longitudeMinutes = (int) minLon;
+        int latitudeMinutes = (int) minLat;
+        double secLon = minLon * 60 - longitudeMinutes * 60;
+        double secLat = minLat * 60 - latitudeMinutes * 60;
+        String formattedLongitude = "" + longitudeDegrees + "°" + longitudeMinutes + "'" + decFormat1.format(secLon) + "\"" + eo;
+        String formattedLatitude = "" + latitudeDegrees + "°" + latitudeMinutes + "'" + decFormat1.format(secLat) + "\"" + ns;
+        return formattedLatitude + " " + formattedLongitude;
     }
 }
